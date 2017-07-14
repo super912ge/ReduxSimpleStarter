c5454efa6ee517830af112320e991689dc3d2a5d
Yiwei Ge <yge@proship.com>
2017-07-14 15:50:45 -0400

    lecture 49

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,12 @@
 import { combineReducers } from 'redux';
+import ActiveBook from './reducer_active_book';
+import BooksReducer from './reducer_books'
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  //state: (state = {}) => state
+
+  books:BooksReducer,
+  activeBook: ActiveBook
 });
 
 export default rootReducer;
